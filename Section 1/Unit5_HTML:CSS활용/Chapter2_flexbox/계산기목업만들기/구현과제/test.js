35a9b67629357c51a9811e9a179306ffb7df1dbf
soulhn <vldnrlf123@naver.com>
2022-12-27 15:50:17 +0900

    계산기 구현

--- Section 1/Unit5_HTML:CSS활용/Chapter2_flexbox/계산기목업만들기/구현과제/test.js
+++ Section 1/Unit5_HTML:CSS활용/Chapter2_flexbox/계산기목업만들기/구현과제/test.js
@@ -10,64 +10,18 @@ function calculate(n1, operator, n2) {
   let result = 0;
   // TODO : n1과 n2를 operator에 따라 계산하는 함수를 만드세요.
   // ex) 입력값이 n1 : '1', operator : '+', n2 : '2' 인 경우, 3이 리턴됩니다.
-  result = eval(n1 + operator + n2);
-  return "" + result;
-}
-
-buttons.addEventListener("click", function (event) {
-  // 버튼을 눌렀을 때 작동하는 함수입니다.
-
-  const target = event.target; // 클릭된 HTML 엘리먼트의 정보가 저장되어 있습니다.
-  const action = target.classList[0]; // 클릭된 HTML 엘리먼트에 클레스 정보를 가져옵니다.
-  const buttonContent = target.textContent; // 클릭된 HTML 엘리먼트의 텍스트 정보를 가져옵니다.
-  // ! 위 코드(Line 19 - 21)는 수정하지 마세요.
-
-  if (target.matches("button")) {
-    // TODO : 계산기가 작동할 수 있도록 아래 코드를 수정하세요. 작성되어 있는 조건문과 console.log를 활용하시면 쉽게 문제를 풀 수 있습니다.
-    // 클릭된 HTML 엘리먼트가 button이면
-    if (action === "number") {
-      // 그리고 버튼의 클레스가 number이면
-      // 아래 코드가 작동됩니다.
-      // console.log("숫자 " + buttonContent + " 버튼");
-      // if (firstOperend.textContent === "0") {
-      //   firstOperend.textContent = buttonContent;
-      // } else if (firstOperend.textContent !== "0") {
-      //   secondOperend.textContent = buttonContent;
-      // } 리팩토링
-      firstOperend.textContent === "0" ? (firstOperend.textContent = buttonContent) : (secondOperend.textContent = buttonContent);
-    }
-
-    if (action === "operator") {
-      // console.log("연산자 " + buttonContent + " 버튼");
-      operator.textContent = buttonContent;
-    }
-
-    if (action === "decimal") {
-      // console.log("소수점 버튼");
-    }
-
-    if (action === "clear") {
-      // console.log("초기화 버튼");
-      firstOperend.textContent = 0;
-      secondOperend.textContent = 0;
-      operator.textContent = "+";
-      calculatedResult.textContent = 0;
-    }
-
-    if (action === "calculate") {
-      // console.log("계산 버튼");
-      calculatedResult.textContent = calculate(firstOperend.textContent, operator.textContent, secondOperend.textContent);
-    }
+  if (operator === "+") {
+    result = Number(n1) + Number(n2);
+  } else if (operator === "-") {
+    result = Number(n1) - Number(n2);
+  } else if (operator === "*") {
+    result = Number(n1) * Number(n2);
+  } else if (operator === "/") {
+    result = Number(n1) / Number(n2);
   }
-});
-
-// ! Advanced Challenge test와 Nightmare test를 위해서는 아래 주석을 해제하세요.
-// 여부터 코드 짜그라~
-const display = document.querySelector(".calculator__display--for-advanced"); // calculator__display 엘리먼트와, 그 자식 엘리먼트의 정보를 모두 담고 있습니다.
-let firstNum;
-let operatorForAdvanced;
-let previousKey;
-let previousNum;
+  console.log(result);
+  return String(result);
+}
 
 buttons.addEventListener("click", function (event) {
   // 버튼을 눌렀을 때 작동하는 함수입니다.
@@ -80,7 +34,49 @@ buttons.addEventListener("click", function (event) {
   // ! 여기서부터 Advanced Challenge & Nightmare 과제룰 풀어주세요.
   if (target.matches("button")) {
     if (action === "number") {
-      if (display.textContent === "0" || previousKey === "operator") {
+      if (firstOperend.textContent != 0) {
+        secondOperend.textContent = buttonContent;
+      } else {
+        firstOperend.textContent = buttonContent;
+      }
+    }
+    if (action === "operator") {
+      operator.textContent = buttonContent;
+    }
+    if (action === "decimal") {
+    }
+    if (action === "clear") {
+      firstOperend.textContent = "0";
+      operator.textContent = "+";
+      secondOperend.textContent = "0";
+      calculatedResult.textContent = "0";
+    }
+    if (action === "calculate") {
+      // let n1 = Number(firstOperend.textContent);
+      // let n2 = Number(secondOperend.textContent);
+      // let operator_n1n2 = operator.textContent;
+      calculatedResult.textContent = calculate(firstOperend.textContent, operator.textContent, secondOperend.textContent);
+    }
+  }
+});
+
+// ! Advanced Challenge test와 Nightmare test를 위해서는 아래 주석을 해제하세요.
+
+const display = document.querySelector(".calculator__display--for-advanced"); // calculator__display 엘리먼트와, 그 자식 엘리먼트의 정보를 모두 담고 있습니다.
+let firstNum, operatorForAdvanced, previousKey, previousNum;
+
+buttons.addEventListener("click", function (event) {
+  // 버튼을 눌렀을 때 작동하는 함수입니다.
+
+  const target = event.target; // 클릭된 HTML 엘리먼트의 정보가 저장되어 있습니다.
+  const action = target.classList[0]; // 클릭된 HTML 엘리먼트에 클레스 정보를 가져옵니다.
+  const buttonContent = target.textContent; // 클릭된 HTML 엘리먼트의 텍스트 정보를 가져옵니다.
+  // ! 위 코드는 수정하지 마세요.
+
+  // ! 여기서부터 Advanced Challenge & Nightmare 과제룰 풀어주세요.
+  if (target.matches("button")) {
+    if (action === "number") {
+      if (display.textContent === "0" || previousKey === "operator" || previousKey === "calculate") {
         display.textContent = buttonContent;
       } else {
         display.textContent += buttonContent;
@@ -88,38 +84,36 @@ buttons.addEventListener("click", function (event) {
       previousKey = "number";
     }
     if (action === "operator") {
-      console.log("연산자 " + buttonContent + " 버튼");
+      target.classList.add("isPressed");
+      if (firstNum && operatorForAdvanced && previousKey !== "operator" && previousKey !== "calculate") {
+        display.textContent = calculate(firstNum, operatorForAdvanced, display.textContent);
+      }
+      firstNum = display.textContent;
       operatorForAdvanced = buttonContent;
       previousKey = "operator";
-      firstNum = display.textContent;
     }
-    // 소수
-    // 여러번 눌러도 단 한번만 입력,
-    // 정수 부분이 없다면 0.5로 등장
     if (action === "decimal") {
-      if (previousKey === "operator") {
-        display.textContent = "0.";
-      } else if (display.textContent.includes(".")) {
-        display.textContent = display.textContent;
-      } else {
+      if (!display.textContent.includes(".") && previousKey != "operator") {
         display.textContent += ".";
+      } else if (previousKey === "operator") {
+        display.textContent = "0.";
       }
 
       previousKey = "decimal";
     }
     if (action === "clear") {
-      firstNum = null;
-      operatorForAdvanced = null;
+      firstNum = undefined;
+      operatorForAdvanced = undefined;
+      previousNum = undefined;
       previousKey = "clear";
       display.textContent = "0";
     }
     if (action === "calculate") {
-      //전에 입력 한값 있는지
       if (firstNum) {
-        //엔터를 눌렀는데 엔터를 누르면? 결과화면값 + 마지막 값 연산
+        // firstNum이 있다면
         if (previousKey === "calculate") {
+          // enter를 연속적으로 눌렀다면
           display.textContent = calculate(display.textContent, operatorForAdvanced, previousNum);
-          //엔터를 마지막으로 누른키가 엔터가 아니면?
         } else {
           previousNum = display.textContent;
           display.textContent = calculate(firstNum, operatorForAdvanced, display.textContent);
