3b39dafdb53560db7df2ac68962aaaa81c6a4255
soulhn <vldnrlf123@naver.com>
2023-01-03 01:13:25 +0900

    add test

---
+++ Section 1/Unit9_JS핵심개념과주요문법/Chapeter2_스코프/스코프실습/scope.js
@@ -0,0 +1,8 @@
+function greetSomeone(firstName) {
+  let time = "night";
+  if (time === "night") {
+    let greeting = "Good Night";
+  }
+  return greeting + " " + firstName;
+}
+greetSomeone("Steve");
