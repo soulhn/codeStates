03259e6abb9fb8a6492ff201b5138acafe7c3840
soulhn <vldnrlf123@naver.com>
2023-01-25 16:58:11 +0900

    add beforePage Btn

--- Section 2/Unit5_React_SPA/fe-sprint-react-twittler-spa-main/src/Sidebar.js
+++ Section 2/Unit5_React_SPA/fe-sprint-react-twittler-spa-main/src/Sidebar.js
@@ -1,8 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+
 // TODO - import문을 이용하여 react-router-dom 라이브러리의 Link 컴포넌트를 불러옵니다.
 
 const Sidebar = () => {
+  const navigate = useNavigate();
   return (
     <section className="sidebar">
       {/* TODO : About 메뉴 아이콘과 Mypage 메뉴 아이콘을 작성하고 Link 컴포넌트를 이용하여 경로(path)를 연결합니다. */}
@@ -15,6 +18,13 @@ const Sidebar = () => {
       <Link to="/mypage">
         <i className="far fa-user"></i>
       </Link>
+      <button
+        onClick={() => {
+          navigate(-1);
+        }}
+      >
+        이전페이지
+      </button>
     </section>
   );
 };
