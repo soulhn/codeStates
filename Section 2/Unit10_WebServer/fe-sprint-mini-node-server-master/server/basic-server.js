67738e3cc714927cc107e035ae16308e3b76f597
soulhn <vldnrlf123@naver.com>
2023-02-06 23:15:20 +0900

    [Add]Mini Server My solution

--- Section 2/Unit10_WebServer/fe-sprint-mini-node-server-master/server/basic-server.js
+++ Section 2/Unit10_WebServer/fe-sprint-mini-node-server-master/server/basic-server.js
@@ -1,15 +1,47 @@
-const http = require('http');
-
+const http = require("http");
 const PORT = 4999;
-
-const ip = 'localhost';
+const ip = "localhost";
 
 const server = http.createServer((request, response) => {
-  console.log(
-    `http request method is ${request.method}, url is ${request.url}`
-  );
-  response.writeHead(200, defaultCorsHeader);
-  response.end('hello mini-server sprints');
+  console.log(`http request method is ${request.method}, url is ${request.url}`);
+  //작성 코드(메서드, URL, HEADER)
+  const { method, url, headers } = request;
+  if (method === "OPTIONS") {
+    response.writeHead(200, defaultCorsHeader);
+    response.end("hello mini-server sprints");
+  }
+  if (method === "POST") {
+    let body = [];
+    request.on("data", (chunk) => {
+      body.push(chunk);
+    });
+    request.on("end", () => {
+      body = Buffer.concat(body).toString();
+      response.writeHead(200, defaultCorsHeader);
+      console.log(body);
+      if (url === "/lower") {
+        response.end(body.toLowerCase());
+      } else if (url === "/upper") {
+        console.log(body.toUpperCase());
+        response.end(body.toUpperCase());
+      } else {
+        response.statusCode = 400;
+        response.end();
+      }
+    });
+  } else {
+    response.statusCode = 400;
+    response.end();
+  }
+
+  // if (request.method === "POST" && request.url === "/lower") {
+  //   console.log("lower요청");
+  //   console.log(request.body);
+  // } else if (request.method === "POST" && request.url === "/upper") {
+  //   console.log("upper요청");
+  // } else {
+  //   console.log("error");
+  // }
 });
 
 server.listen(PORT, ip, () => {
@@ -17,8 +49,8 @@ server.listen(PORT, ip, () => {
 });
 
 const defaultCorsHeader = {
-  'Access-Control-Allow-Origin': '*',
-  'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
-  'Access-Control-Allow-Headers': 'Content-Type, Accept',
-  'Access-Control-Max-Age': 10
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+  "Access-Control-Allow-Headers": "Content-Type, Accept",
+  "Access-Control-Max-Age": 10,
 };
