719c055bc599f25630bb96ac770bfda2e0663d81
soulhn <vldnrlf123@naver.com>
2023-02-08 17:05:33 +0900

    [Add]States solution

--- Section 2/Unit10_WebServer/fe-sprint-statesairline-server-master/statesairline/controller/bookController.js
+++ Section 2/Unit10_WebServer/fe-sprint-statesairline-server-master/statesairline/controller/bookController.js
@@ -1,5 +1,5 @@
 // POST /book에서 사용할 uuid입니다.
-const { v4: uuid } = require('uuid');
+const { v4: uuid } = require("uuid");
 // 항공편 예약 데이터를 저장합니다.
 let booking = [];
 
@@ -12,14 +12,25 @@ module.exports = {
   // [GET] /book/:phone 요청을 수행합니다.
   // 요청 된 phone과 동일한 phone 예약 데이터를 조회합니다.
   findByPhone: (req, res) => {
-    const {phone} = req.params;
+    const { phone } = req.params;
 
+    const result = booking.filter((currentBooking) => {
+      return currentBooking.phone === phone;
+    });
+
+    return res.json(result);
   },
   // [GET] /book/:phone/:flight_uuid 요청을 수행합니다.
   // 요청 된 id, phone과 동일한 uuid, phone 예약 데이터를 조회합니다.
-  findByPhoneAndFlightId: (req,res) => {
-    const {phone, flight_uuid} = req.params;
+  findByPhoneAndFlightId: (req, res) => {
+    const { phone, flight_uuid } = req.params;
     // TODO:
+
+    const result = booking.filter((currentBooking) => {
+      return currentBooking.phone === phone && currentBooking.flight_uuid === flight_uuid;
+    });
+
+    return res.json(result);
   },
   // [POST] /book 요청을 수행합니다.
   // 요청 된 예약 데이터를 저장합니다.
@@ -27,13 +38,25 @@ module.exports = {
     // POST /book에서 사용할 booking_uuid입니다.
     const booking_uuid = uuid();
     // TODO:
+    const { flight_uuid, name, phone } = req.body;
+
+    const newBooking = {
+      booking_uuid,
+      flight_uuid,
+      name,
+      phone,
+    };
+
+    booking.push(newBooking);
+
+    return res.json(newBooking);
   },
 
   // Optional
   // [DELETE] /book/:booking_uuid 요청을 수행합니다.
   // 요청 된 id, phone 값과 동일한 예약 데이터를 삭제합니다.
   deleteByBookingId: (req, res) => {
-    const {booking_uuid} = req.params;
+    const { booking_uuid } = req.params;
     // TODO:
-  }
+  },
 };
--- Section 2/Unit10_WebServer/fe-sprint-statesairline-server-master/statesairline/controller/flightController.js
+++ Section 2/Unit10_WebServer/fe-sprint-statesairline-server-master/statesairline/controller/flightController.js
@@ -1,15 +1,38 @@
-const flights = require('../repository/flightList');
-const fs = require('fs');
+const flights = require("../repository/flightList");
+const fs = require("fs");
+
+const queryCheck = (flight, departure_times, arrival_times, destination, departure) => {
+  if (departure_times && flight.departure_times !== departure_times) {
+    return false;
+  }
+
+  if (arrival_times && flight.arrival_times !== arrival_times) {
+    return false;
+  }
+
+  if (destination && flight.destination !== destination) {
+    return false;
+  }
+
+  if (departure && flight.departure !== departure) {
+    return false;
+  }
+
+  return true;
+};
 
 module.exports = {
   // [GET] /flight
   // 요청 된 파라미터 departure_times, arrival_times 값과 동일한 값을 가진 항공편 데이터를 조회합니다.
   // 요청 된 파라미터 departure, destination 값과 동일한 값을 가진 항공편 데이터를 조회합니다.
+
   findAll: (req, res) => {
     const { departure_times, arrival_times, destination, departure } = req.query;
     // TODO:
 
-    return res.json(flights);
+    const result = flights.filter((flight) => queryCheck(flight, departure_times, arrival_times, destination, departure));
+
+    return res.json(result);
   },
   // [GET] /flight/:uuid
   // 요청 된 uuid 값과 동일한 uuid 값을 가진 항공편 데이터를 조회합니다.
@@ -17,6 +40,9 @@ module.exports = {
     const { uuid } = req.params;
     // TODO:
 
+    const result = flights.filter((flight) => flight.uuid === uuid);
+
+    return res.json(result);
   },
 
   // Advanced
@@ -25,6 +51,6 @@ module.exports = {
   update: (req, res) => {
     const { uuid } = req.params;
     const bodyData = req.body;
-     // TODO:
-  }
+    // TODO:
+  },
 };
