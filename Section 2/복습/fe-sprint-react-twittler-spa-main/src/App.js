d1464f7e8239eca745da0bcd6c2667b9f3a47812
soulhn <vldnrlf123@naver.com>
2023-02-12 21:09:00 +0900

    feat: part2 정답 추가

--- Section 2/복습/fe-sprint-react-twittler-spa-main/src/App.js
+++ Section 2/복습/fe-sprint-react-twittler-spa-main/src/App.js
@@ -1,25 +1,33 @@
-import React from 'react';
-import './App.css';
-import './global-style.css';
+import React from "react";
+import "./App.css";
+import "./global-style.css";
 // TODO - react-router-dom을 설치 후, import 구문을 이용하여 BrowserRouter, Routes, Route 컴포넌트를 불러오세요.
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Sidebar from './Sidebar';
-import Tweets from './Pages/Tweets';
-// TODO - import문을 이용하여 MyPage, About 컴포넌트를 불러오세요.
+import Sidebar from "./Sidebar";
+import Tweets from "./Pages/Tweets";
+import About from "./Pages/About";
+import MyPage from "./Pages/MyPage";
 
 const App = () => {
   return (
     <div>
       {/* TODO - BrowserRouter 컴포넌트를 작성합니다. */}
-      <div className="App">
-        <main>
-          <Sidebar />
-          <section className="features">
-            {/* TODO - Routes와 Route 컴포넌트를 이용하여 경로(path)를 설정하고 Tweets, Mypage, About 컴포넌트를 연결합니다. */}
-            <Tweets />
-          </section>
-        </main>
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <main>
+            <Sidebar />
+            <section className="features">
+              {/* TODO - Routes와 Route 컴포넌트를 이용하여 경로(path)를 설정하고 Tweets, Mypage, About 컴포넌트를 연결합니다. */}
+              <Routes>
+                <Route path="/" element={<Tweets />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/mypage" element={<MyPage />} />
+              </Routes>
+            </section>
+          </main>
+        </div>
+      </BrowserRouter>
     </div>
   );
 };
