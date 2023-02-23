a1c8494d1d84e8a6420a57b0bdeffbb81d6906ce
soulhn <vldnrlf123@naver.com>
2023-02-23 15:39:34 +0900

    feat: Cmarket Hooks 첫번째 풀이

---
+++ Section3/Unit4_상태관리/fe-sprint-cmarket-hooks-master/src/pages/ItemListContainer.js
@@ -0,0 +1,30 @@
+import React from "react";
+import Item from "../components/Item";
+
+function ItemListContainer({ items, cartItems, setCartItems }) {
+  const handleClick = (item) => {
+    const copyCartItems = cartItems;
+    const isIncart = cartItems.filter((el) => el.itemId === item.id)[0];
+    if (isIncart) {
+      const idx = copyCartItems.indexOf(isIncart);
+      copyCartItems[idx].quantity += 1;
+      setCartItems(copyCartItems);
+    } else {
+      setCartItems([...cartItems, { itemId: item.id, quantity: 1 }]);
+    }
+
+    console.log(isIncart);
+  };
+  return (
+    <div id="item-list-container">
+      <div id="item-list-body">
+        <div id="item-list-title">쓸모없는 선물 모음</div>
+        {items.map((item, idx) => (
+          <Item item={item} key={idx} handleClick={() => handleClick(item)} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default ItemListContainer;
