d9844fb200a30389f18a61688c7d53b7348e8fd0
soulhn <vldnrlf123@naver.com>
2023-02-27 21:17:44 +0900

    feat: cmarket-redux solution

--- Section3/Unit4_상태관리/fe-sprint-cmarket-redux-master/src/reducers/itemReducer.js
+++ Section3/Unit4_상태관리/fe-sprint-cmarket-redux-master/src/reducers/itemReducer.js
@@ -2,24 +2,30 @@ import { REMOVE_FROM_CART, ADD_TO_CART, SET_QUANTITY } from "../actions/index";
 import { initialState } from "./initialState";
 
 const itemReducer = (state = initialState, action) => {
-
   switch (action.type) {
     case ADD_TO_CART:
       //TODO
+      return Object.assign({}, state, {
+        cartItems: [...state.cartItems, action.payload],
+      });
 
-      break;
     case REMOVE_FROM_CART:
       //TODO
+      let currentItem = state.cartItems.filter((el) => el.itemId !== action.payload.itemId);
+      return Object.assign({}, state, {
+        cartItems: currentItem,
+      });
 
-      break;
     case SET_QUANTITY:
-      let idx = state.cartItems.findIndex(el => el.itemId === action.payload.itemId)
+      let idx = state.cartItems.findIndex((el) => el.itemId === action.payload.itemId);
       //TODO
-
-      break;
+      return {
+        ...state,
+        cartItems: [...state.cartItems.slice(0, idx), action.payload, ...state.cartItems.slice(idx + 1)],
+      };
     default:
       return state;
   }
-}
+};
 
 export default itemReducer;
