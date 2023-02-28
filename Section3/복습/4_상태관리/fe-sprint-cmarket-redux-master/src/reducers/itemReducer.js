f1c3d31ca87c773cdfba9384cceb82a00a5211f4
soulhn <vldnrlf123@naver.com>
2023-02-28 22:39:53 +0900

    feat: redux복습 완료 코드

--- Section3/복습/4_상태관리/fe-sprint-cmarket-redux-master/src/reducers/itemReducer.js
+++ Section3/복습/4_상태관리/fe-sprint-cmarket-redux-master/src/reducers/itemReducer.js
@@ -2,24 +2,38 @@ import { REMOVE_FROM_CART, ADD_TO_CART, SET_QUANTITY } from "../actions/index";
 import { initialState } from "./initialState";
 
 const itemReducer = (state = initialState, action) => {
+  // {
+  //   type: ADD_TO_CART,
+  //   payload: {
+  //     itemId,
+  //   },
+  // };
 
   switch (action.type) {
     case ADD_TO_CART:
       //TODO
+      return { ...state, cartItems: [...state.cartItems, action.payload] };
 
-      break;
     case REMOVE_FROM_CART:
       //TODO
+      return { ...state, cartItems: state.cartItems.filter((el) => el.itemId !== action.payload.itemId) };
 
-      break;
     case SET_QUANTITY:
-      let idx = state.cartItems.findIndex(el => el.itemId === action.payload.itemId)
+      let targetIdx = state.cartItems.findIndex((el) => el.itemId === action.payload.itemId);
       //TODO
-
-      break;
+      return {
+        ...state,
+        cartItems: state.cartItems.map((el, idx) => {
+          if (idx === targetIdx) {
+            return action.payload;
+          } else {
+            return el;
+          }
+        }),
+      };
     default:
       return state;
   }
-}
+};
 
 export default itemReducer;
