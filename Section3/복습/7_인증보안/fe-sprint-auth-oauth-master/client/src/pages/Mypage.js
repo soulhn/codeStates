38eefff9b8f67deea4b7af8d9e56c24165251c71
soulhn <vldnrlf123@naver.com>
2023-04-17 17:21:48 +0900

    feat: OAuth 복습

--- Section3/복습/7_인증보안/fe-sprint-auth-oauth-master/client/src/pages/Mypage.js
+++ Section3/복습/7_인증보안/fe-sprint-auth-oauth-master/client/src/pages/Mypage.js
@@ -1,9 +1,9 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react';
-import Loading from './components/Loading';
-import User from './components/UserInfo';
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import Loading from "./components/Loading";
+import User from "./components/UserInfo";
 
-export default function Mypage(/*TODO: 컴포넌트에 필요한 props를 전달하세요.*/) {
+export default function Mypage({ accessToken, setIsLogin, setAccesToken }) {
   const [githubUser, setGithubUser] = useState(null);
   const [serverResource, setServerResource] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
@@ -12,6 +12,24 @@ export default function Mypage(/*TODO: 컴포넌트에 필요한 props를 전달
     // TODO: /logout을 통해 사용자가 로그아웃되도록 구현하세요.
     // prop으로 받은 Access Token을 이용해 /logout 엔드포인트로 요청을 보내야합니다.
     // 요청이 성공했다면 isLogin 상태를 false로 업데이트해야 합니다.
+    axios
+      .delete("http://localhost:4000/logout", {
+        headers: {
+          "Content-Type": "application/json",
+        },
+        data: { accessToken },
+      })
+      .then((res) => {
+        setIsLogin(false);
+        setAccesToken(null);
+        setGithubUser(null);
+        setServerResource(null);
+        setIsLoading(false);
+      })
+      .catch(() => {
+        setIsLogin(false);
+        setAccesToken(null);
+      });
   };
 
   useEffect(() => {
@@ -19,11 +37,17 @@ export default function Mypage(/*TODO: 컴포넌트에 필요한 props를 전달
     // prop으로 받은 Access Token을 이용해 /userinfo 엔드포인트로 요청을 보내야합니다.
     // 응답으로 받은 데이터를 githubUser, serverResource의 상태로 업데이트해야합니다.
     // isLoading 상태를 false로 업데이트해야 합니다.
+    axios.post("http://localhost:4000/userinfo", { accessToken }).then((res) => {
+      const { githubUserData, serverResource } = res.data;
+      setGithubUser(githubUserData);
+      setServerResource(serverResource);
+      setIsLoading(false);
+    });
   }, []);
 
   return (
     <>
-      <div className='left-box'>
+      <div className="left-box">
         {!isLoading && (
           <span>
             {`${githubUser.login}`}님,
@@ -31,18 +55,8 @@ export default function Mypage(/*TODO: 컴포넌트에 필요한 props를 전달
           </span>
         )}
       </div>
-      <div className='right-box'>
-        <div className='input-field'>
-          {isLoading ? (
-            <Loading />
-          ) : (
-            <User
-              githubUser={githubUser}
-              serverResource={serverResource}
-              logoutHandler={logoutHandler}
-            />
-          )}
-        </div>
+      <div className="right-box">
+        <div className="input-field">{isLoading ? <Loading /> : <User githubUser={githubUser} serverResource={serverResource} logoutHandler={logoutHandler} />}</div>
       </div>
     </>
   );
